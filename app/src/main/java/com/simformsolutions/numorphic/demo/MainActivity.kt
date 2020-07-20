9380b7b38d4fb8b9a04fc52a7d909cd186cda9e3
Prem Patel <prem.p@simformsolutions.com>
2020-07-20 17:14:30 +0530

    NumorphImageView

--- app/src/main/java/com/simformsolutions/numorphic/demo/MainActivity.kt
+++ app/src/main/java/com/simformsolutions/numorphic/demo/MainActivity.kt
@@ -3,14 +3,14 @@ package com.simformsolutions.numorphic.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
-import com.simformsolutions.numorphic.component.NumorphImageButton
+import com.simformsolutions.numorphic.component.NumorphImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val v = findViewById<NumorphImageButton>(R.id.imageButton)
+        val v = findViewById<NumorphImageView>(R.id.imageButton)
         Glide.with(v.context)
             .load(R.drawable.profile)
             .circleCrop()
