fb9041a30d6970f070f5d72f1fcf7ee3af05d738
Prem Patel <prem.p@simformsolutions.com>
2020-08-11 11:56:39 +0530

    Update : Demo view

---
+++ demo/src/main/java/com/simformsolutions/ssneumorphic/demo/DemoActivity.kt
@@ -0,0 +1,12 @@
+package com.simformsolutions.ssneumorphic.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
